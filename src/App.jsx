072f8e634d902215a98e03f46b78e381fc4f754c
NalagarlaVamsikrishna <nalagarlavamsikrishna3@gmail.com>
2025-10-13 18:54:28 +0530

    Second commit TaskList

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Taskform from './Taskform'
 import Tasklist from './Tasklist'
 import Progresstracker from './Progresstracker'
 
 export default function App() {
+  const [tasks, settasks] = useState([]);
+  
+  useEffect(() => {
+   localStorage.setItem("tasks", JSON.stringify(tasks));
+  })
+
+  const addTask = (task) => {
+    settasks([...tasks,task]);
+  }
+
   return (
     <div>
       <h1>Daily Progress</h1>
       <p>Let's do our work</p>
-      <Taskform />
-      <Tasklist />
+      <Taskform addTask={addTask}/>
+      <Tasklist tasks = {tasks} 
+      updateTask = {updateTask}
+      deleteTask = {deleteTask}/>
       <Progresstracker />
       <button>Clear all Components</button>
     </div>
--- src/Taskform.jsx
+++ src/Taskform.jsx
@@ -1,31 +1,40 @@
 import React, { useState } from 'react'
 
-export default function Taskform() {
+export default function Taskform({addTask}) {
   const [task, setTask] = useState('');
   const[priority, setPriority] = useState('medium');
   const[category, setCategory] = useState('general');
+
+  const handlesubmit = (e) => {
+    e.preventDefault();
+    addTask({text: task, priority, category, completed: false});
+    //reset
+    setTask('');
+    setPriority("medium");
+    setCategory("general");
+  }
   return (
-      <form id='task-form'> 
+      <form onSubmit={handlesubmit} id='task-form'> 
       <div id='inp'>
         <input type="text" placeholder='Enter the task'
-        onChange={(e)=>setTask(e.target.value)}/>
+        value = {task} onChange={(e)=>setTask(e.target.value)}/>
         <button type='submit'>Add Task</button>
         {/*<h1>{task}</h1> */}
       </div>
 
       <div id='btns'>
-        <select onChange={(e)=> setPriority(e.target.value)}>
+        <select value = {priority} onChange={(e)=> setPriority(e.target.value)}>
             <option value="easy">Easy</option>
             <option value="medium">Medium</option>
             <option value="hard">Hard</option>
         </select>
 
-        <select onChange={(e)=> setCategory(e.target.value)}>
+        <select value = {category} onChange={(e)=> setCategory(e.target.value)}>
           <option value="general">General</option>
           <option value="work">Work</option>
           <option value="personal">Personal</option>
         </select>
-        {/*<h1>{task} {priority} {category}</h1>*/}
+        {<h1>{task} {priority} {category}</h1>}
         </div>
       </form>
 
